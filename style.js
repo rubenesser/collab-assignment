7d60cf6416bc2cb3017325d86c1aaecafee961f2
OliviaVG <oliviavangerrevink@gmail.com>
2017-07-03 20:58:40 +0200

    adding Valencia

---
+++ style.js
@@ -0,0 +1,70 @@
+.back-btn {
+  font-size: 1.5em;
+  font-weight: bolder;
+  vertical-align: middle;
+}
+
+//*.windowed .hero-body, .city-tile .mesh {
+  background-image: url('images/black-mesh.png');
+  background-repeat: repeat;
+}*//
+
+.city-tile {
+  min-height: 300px;
+  cursor: pointer;
+}
+
+.city-tile .full-block {
+  width: 100%;
+  height: 100%;
+}
+
+.city-tile .mesh {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.city-tile h2 {
+  color: white;
+}
+
+#rissani-bg {
+  background-image: url('images/rissani1.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+#valenciabackground{
+  background-image: url('images/valencia3.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: 800px;
+  background-color: white;
+
+}
+.valenciatitle{
+  color: black;
+}
+.valenciasubtitle{
+  color: black;
+  padding:10px;
+	font-weight:bold;
+	text-align: center;
+	font-size: 20px
+}
+.form1{
+
+  margin: 10px 200px 200px 600px;
+
+}
+.drink{
+
+}
+.fruit{
+
+}
+.food{
+
+}
